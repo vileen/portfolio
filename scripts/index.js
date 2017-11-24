fbe5b2649767d6b184a07864a8714fee4571ecbc
vileen <soczewka.dominik@gmail.com>
2017-11-24 21:46:33 +0100

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// $(document).ready(function() {
+//     var image = new Image();
+//
+//     image.onload = function() {
+//         $("#profile-picture").attr('src', this.src);
+//     };
+//
+//     image.src = 'https://graph.facebook.com/1748779186/picture?type=large';
+//
+//     $('#form-send').on('click', function(e) {
+//         e.preventDefault();
+//         e.stopPropagation();
+//     })
+// });
